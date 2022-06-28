bdfae385c0e7a326d9fcc2fd458a243f59f49e28
Liza Patt <pattmorokot@gmail.com>
2022-06-29 02:38:06 +0700

    Initial Commit

---
+++ wavesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wavesApp
+//
+//  Created by Liza Patt on 29/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
