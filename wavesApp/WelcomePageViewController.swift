c4d657d5d617b0b32b2afa2f06256686e0eaa44b
Liza Patt <pattmorokot@gmail.com>
2022-06-30 00:53:59 +0700

    added segues

---
+++ wavesApp/WelcomePageViewController.swift
@@ -0,0 +1,49 @@
+//
+//  WelcomePageViewController.swift
+//  wavesApp
+//
+//  Created by Liza Patt on 29/6/22.
+//
+
+import UIKit
+
+class WelcomePageViewController: UIViewController {
+
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+   
+
+    @IBAction func timer(_ sender: UIButton) {
+    }
+    
+    @IBAction func reflection(_ sender: UIButton) {
+    }
+    
+    @IBAction func community(_ sender: UIButton) {
+    }
+    
+    @IBAction func resources(_ sender: UIButton) {
+    }
+    
+    
+ /*
+  
+     // MARK: - Navigation
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        
+        }
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+
+*/
+
+}
