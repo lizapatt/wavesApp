9c37bf9ffcc2e4c83557ce71b5468dc4fb6f0941
Liza Patt <pattmorokot@gmail.com>
2022-06-30 01:59:30 +0700

    added some stuff

---
+++ wavesApp/reflectionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  reflectionViewController.swift
+//  wavesApp
+//
+//  Created by Liza Patt on 30/6/22.
+//
+
+import UIKit
+
+class reflectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
