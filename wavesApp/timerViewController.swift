11f8b4b60fc7da07c7f73706ac19aed4eb751762
Deyse <deysest.mary@gmail.com>
2022-06-30 16:50:07 -0400

    Here

--- wavesApp/timerViewController.swift
+++ wavesApp/timerViewController.swift
@@ -1,21 +1,104 @@
-//
-//  timerViewController.swift
-//  wavesApp
-//
-//  Created by Liza Patt on 30/6/22.
-//
-
 import UIKit
 
-class timerViewController: UIViewController {
-
-    override func viewDidLoad() {
+class timerViewController:UIViewController
+{
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var pauseButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var plusfiveButton: UIButton!
+    
+    @IBOutlet weak var minusfiveButton: UIButton!
+    
+    var timer = Timer()
+    var seconds = 60
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    }
+    @IBAction func startbutton(_ sender: Any)
+    {
+        //Make sure there arn't any other timers running
+        timer.invalidate()
+        //Create the timer
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self,  selector: #selector(ViewController.timerClass),  userInfo: nil, repeats: true)
+    }
+    @IBAction func pausebutton(_ sender: Any)
+    {
+        timer.invalidate()
+    }
+    @IBAction func resetbutton(_ sender: Any)
+    {
+        timer.invalidate()
+        seconds = 60
+        timerLabel.text = String(seconds)
+    }
+    @IBAction func plusfivebutton(_ sender: Any)
+    {
+        seconds = seconds + 5
+        timerLabel.text = String(seconds)
+        
+    }
+    @IBAction func minusfivebutton(_ sender:Any)
+    {
+        seconds = seconds - 5
+        timerLabel.text = String(seconds)
+    }
+    @objc func timerClass()
+    {
+        seconds -= 1
+        timerLabel.text = String(seconds)
+        
+        if(seconds == 0)
+        {
+            timer.invalidate()
+        }
     }
     
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+    
+    
+    
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
     
     // MARK: - Navigation
 /*
